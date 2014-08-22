a981a9c2d016d98949242c1a5a321ec364b4b8d1
Joyzyx <Joyzyx@users.noreply.github.com>
2014-08-23 02:07:52 +0800

    Update cachematrix.R
    figure out the relationship between the red part and the green part.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and their may be some benefit  
+## to caching the inverse of a matrix rather than compute it repeatedly。 
+## I write a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        invm <- NULL
+        set <- 
+        get <- function() x
+        setinvm <- function(y){
+                x <<- y
+                invm <<- NULL
+        }
+        getinvm <- function() invm
+        list(set = set, get = get,
+             setinvm = setinvm,
+             getinvm = getinvm)
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already been 
+## calculated (and the matrix has not changed), then the cachesolve should 
+## retrieve the inverse from the cache. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invm <- x$getinvm()
+        if(!is.null(invm)){
+                message("getting cached data")
+                return(invm)
+        }
+        data <- x$get()
+        invm <- solve(data, ...)
+        x$setinvm(invm)
+        invm   
 }
